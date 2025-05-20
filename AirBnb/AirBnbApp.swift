ff3dee563f1a7a0e8d0a519956d1b4524c4bba46
DanielJ06 <daniel.j.rodrigues06@gmail.com>
2025-05-20 14:14:25 -0300

    Initial Commit

---
+++ AirBnb/AirBnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbApp.swift
+//  AirBnb
+//
+//  Created by Daniel Rodrigues on 20/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
